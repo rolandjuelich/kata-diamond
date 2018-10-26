0468a17cf338130f2e877d88660522e1302ab688
Roland Jülich <rolandjuelich@gmail.com>
2018-10-26 16:02:31 +0200

    red

---
+++ src/test/java/my/kata/diamond/DiamondConstructionTest.java
@@ -0,0 +1,23 @@
+package my.kata.diamond;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiamondConstructionTest {
+
+	/*
+	 * 	Rules:
+	 * 	------
+	 * 
+	 * 		1. possible letters are a list of characters with defined order and a zero based index
+	 *		2. number of leading or trailing whitespaces is equal to the difference of the indices between A and supplied letter
+	 *		3. number of inner whitespaces is index of current letter +1 or 0 if current letter is 'A'
+	 * 
+	 */
+	
+	@Test
+	public void defineAllowedLetters() {
+		assertThat(allowedLetters()).isInstanceOf(String.class);
+	}
+}
