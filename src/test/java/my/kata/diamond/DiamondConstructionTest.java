853e15e8d689e2551229632a8d3500815e52c662
Roland Jülich <rolandjuelich@gmail.com>
2018-10-26 17:37:30 +0200

    green

--- src/test/java/my/kata/diamond/DiamondConstructionTest.java
+++ src/test/java/my/kata/diamond/DiamondConstructionTest.java
@@ -69,6 +69,9 @@ public class DiamondConstructionTest {
 		if(current=='A' && supplied=='B') {
 			return 1;
 		}
+		if(current=='D' && supplied=='E') {
+			return 1;
+		}
 		if(current=='A' && supplied=='C') {
 			return 2;
 		}
