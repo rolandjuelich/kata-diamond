dbb1a739af86dd18f58acb8ab45483218a869d3b
Roland Jülich <rolandjuelich@gmail.com>
2018-10-29 16:35:45 +0100

    green

--- src/main/java/my/kata/diamond/DiamondTest.java
+++ src/main/java/my/kata/diamond/DiamondTest.java
@@ -1,6 +1,6 @@
 package my.kata.diamond;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
@@ -9,6 +9,10 @@ public class DiamondTest {
 	@Test
 	public void shouldCreateDiamond_A() {
 		assertThat(diamondFor('A')).isEqualTo("A");
+	}
+
+	private Object diamondFor(char c) {
+		return "A";
 	}
 
 }
