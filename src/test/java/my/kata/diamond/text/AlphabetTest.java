9579d5ad45731fe4de68a58738bb3e5b436bdab3
Roland Jülich <rolandjuelich@gmail.com>
2018-10-30 11:58:04 +0100

    red

--- src/test/java/my/kata/diamond/text/AlphabetTest.java
+++ src/test/java/my/kata/diamond/text/AlphabetTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 import my.kata.diamond.text.Alphabet.Letter;
@@ -29,7 +30,7 @@ public class AlphabetTest {
 	public void shouldNotCreateLetterIfNotInAlphabet() {
 		// assume
 		final Alphabet alphabet = Alphabet.of("A");
-		final List<Character> invalid = Arrays.asList('B', 'C', 'D');
+		final List<Character> invalid = Arrays.asList(someCharacterNotIn(alphabet), 'C', 'D');
 
 		for (Character invalidCharacter : invalid) {
 			// given
@@ -46,6 +47,14 @@ public class AlphabetTest {
 					character, alphabet.characters());
 		}
 
+	}
+
+	private static char someCharacterNotIn(final Alphabet alphabet) {
+		String someChar;
+		do {
+			someChar = RandomStringUtils.randomAlphanumeric(1);
+		} while (alphabet.characters().contains(someChar));
+		return someChar.charAt(0);
 	}
 
 }
