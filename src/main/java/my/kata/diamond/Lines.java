6c9c642ac746feceedaa1e2871abe70fe7e03bab
Roland Jülich <rolandjuelich@gmail.com>
2018-10-30 07:47:39 +0100

    green

--- src/main/java/my/kata/diamond/Lines.java
+++ src/main/java/my/kata/diamond/Lines.java
@@ -1,19 +1,30 @@
 package my.kata.diamond;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lines {
 
-	public void add(final Lines firstLine) {
-		
-	}
+	private final List<String> values = new ArrayList<String>();
 
-	public static Lines of(final List<String> firstLine) {
-		return null;
+	private Lines(final List<String> values) {
+		this.values.addAll(values);
 	}
 
 	public static Lines empty() {
-		return new Lines();
+		return new Lines(new ArrayList<String>());
+	}
+	
+	public static Lines of(final List<String> lines) {
+		return new Lines(lines);
+	}
+
+	public Lines add(final Lines lines) {
+		return Lines.of(lines.values());
+	}
+
+	public List<String> values() {
+		return this.values;
 	}
 
 }
