eb8c1a6ffe9a8b1fb46900939d2c6776df1f1c37
Roland Jülich <rolandjuelich@gmail.com>
2018-10-30 11:24:08 +0100

    refactor

--- src/test/java/my/kata/diamond/text/AlphabetTest.java
+++ src/test/java/my/kata/diamond/text/AlphabetTest.java
@@ -4,11 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
+import my.kata.diamond.text.Alphabet.Letter;
+
 public class AlphabetTest {
 
 	@Test
 	public void shouldCreateLetter() {
-		assertThat(Alphabet.of("A").letter('A').value()).isEqualTo('A');
+		// given
+		final Alphabet alphabet = Alphabet.of("A");
+
+		// when
+		final Letter letter = alphabet.letter('A');
+
+		// then
+		assertThat(letter).isInstanceOf(Letter.class);
+		assertThat(letter.value()).isEqualTo('A');
 	}
-	
+
 }
